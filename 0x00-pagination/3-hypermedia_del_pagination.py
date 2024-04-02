cdfc48e92a1076dcb614204cb47bf4664ea22510
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-02 13:23:01 +0100

    fix: add function doc

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """get hypermedia data - deletion resilent"""
         dataset = self.indexed_dataset()
         assert index < len(dataset)
         data = [dataset[idx]
