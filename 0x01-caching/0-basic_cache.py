3d1d8451c4589a65fbad9027bd4a67e7c5b2722b
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-03 17:18:34 +0100

    add: first task 01

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Basic Cache
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic dictionary cache"""
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if not (key or item):
+            return
+        self.cache_data[key] = item
