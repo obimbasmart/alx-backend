7189ae43d61079ee974c93347350e17ed439ab4f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-11 04:27:06 +0100

    add: force locale in url_param

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+
+"""Get locale from request
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Basic flask configurations"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Selector function"""
+    locale = request.args.get("locale")
+    if not locale:
+        return babel.default_locale
+
+    if locale in [_locale.language for _locale in babel.list_translations()]:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Basic route"""
+    return render_template("4-index.html",
+                           home_title="Welcome to Holberton",
+                           home_header="Hello world")
+
+
+if __name__ == "__main__":
+    app.run()
