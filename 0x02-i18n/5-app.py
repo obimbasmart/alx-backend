2f0177c0060ca5e5dcb969cddd96ce4c9dbdbaa7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-11 07:09:49 +0100

    Mock loging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 
-"""Get locale from request
+"""Mock logging in
 """
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -34,12 +41,32 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+def get_user(login_as=None):
+    """get user by ID"""
+    if not login_as:
+        return None
+
+    user = users.get(login_as)
+    if user:
+        return users
+
+
+@app.before_request
+def before_request():
+    """---- ----- ----"""
+    user = None
+    user_id = request.args.get("login_as")
+    if user_id and user_id.isdigit():
+        user = users.get(int(user_id))
+    g.user = user
+
+
 @app.route("/")
 def index():
     """Basic route"""
-    return render_template("4-index.html",
+    return render_template("5-index.html",
                            home_title="Welcome to Holberton",
-                           home_header="Hello world")
+                           home_header="Hello world", user=g.user)
 
 
 if __name__ == "__main__":
